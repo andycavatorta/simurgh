26289e2370832de681ad18e11f8f7291ea382c7f
andycavatorta <andycavatorta@gmail.com>
2015-07-23 11:33:40 -0400

    added gitSetup.py

---
+++ gitSetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.name andycavatorta
+git config --global user.email andycavatorta@gmail.com
+git config --global color.ui auto
+git clone https://github.com/andycavatorta/simurgh.git
+git remote add upstream https://github.com/andycavatorta/simurgh.git
