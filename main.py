de373b48322404641f7045c67df29cd591a78a43
andycavatorta <andycavatorta@gmail.com>
2015-07-27 14:37:16 -0400

    =more

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-print "+++++++++++++"
+"""
 
-from common import  emailIp
+
+
+"""
+import commands
+import socket
+
+cmd = "ip addr list eth0 |grep \"inet \" |cut -d' ' -f6|cut -d/ -f1"
+resp = commands.getstatusoutput(cmd)
+print resp
+IP = resp[1]
+HOSTNAME = socket.gethostname()
+
+if IP:
+    # git pull
+    cmd = "cd /home/pi/simurgh/ && git pull -q --all -p"
+    resp = commands.getstatusoutput(cmd)
+    print resp
+    # email IP
+    from common import  emailIp
+else:
+    pass
+    # assign static IP
+
+
 
